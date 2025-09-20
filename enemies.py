5ede10f41bb98053f8e41642b243057ae65d24e0
matan-11:08=5635-why? <diz.matan@gmail.com>
2025-09-20 23:41:19 +0300

    i created thw point as a object and try to add har to the main but faill

---
+++ enemies.py
@@ -0,0 +1,12 @@
+import pygame
+
+class enemies:
+    pygame.display.set_caption("enemies")
+
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+
+
+    def saze(x,y,radius,color):
+        return x,y,radius
+
--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import sys
 
 pygame.display.flip()
 
+enemies = enemies()
 
 pygame.quit()
 sys.exit()
 
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
 
 pygame.display.flip()
