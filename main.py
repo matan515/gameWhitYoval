d50e3d5d52a4dbecb19fd612caab06616bf28fb9
matan-11:08=5635-why? <diz.matan@gmail.com>
2025-09-20 23:26:40 +0300

    displyWindow

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+pygame.display.flip()
+
+
+pygame.quit()
+sys.exit()
+
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+
+
+pygame.display.flip()
+
+
+pygame.quit()
+sys.exit()
