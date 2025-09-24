61e9dd2fdffe6f35540cca179ae20ebca8be440f
matan <diz.matan@gmail.com>
2025-09-25 01:09:06 +0300

    i finaly fins

--- enemies.py
+++ enemies.py
@@ -1,12 +1,35 @@
-import pygame
+import Utils
+import pygame as pg
+import math
 
-class enemies:
-    pygame.display.set_caption("enemies")
+class Player:
 
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
+    def __init__(self):
+        self.health = 100
+        self.stamina = 100
+        self.x_pos = 250
+        self.y_pos = 200
+        self.range = 10
+        self.punch_cooldown = 0 #in frames
+        self.player_x=x
+        self.player_y=y
 
+    def take_damage(self, damage=5):
+        self.health -= damage
 
-    def saze(x,y,radius,color):
-        return x,y,radius
+    def move(self):
+        direction = math.sqrt((self.player_x - self.x_pos) ** 2 + (self.player_y - self.y_pos) ** 2)
+        self.x_pos += Utils.vector_to_x_y(direction, self.stamina)[0]
+        self.y_pos += Utils.vector_to_x_y(direction, self.stamina)[1]
 
+    def punch(self, enemies):
+        if self.punch_cooldown <= 0:
+            self.punch_cooldown += 72 - (self.stamina / 100)
+            self.stamina -= 20
+            for enemy in enemies:
+                if self.range <= Utils.distance([self.x_pos, self.y_pos], [enemy.x_pos, enemy.y_pos]):
+                    enemy.take_damage()
+
+    def update(self):
+        self.punch_cooldown -= 1
+        self.stamina += 1
